485dabc549920de0cd65da16e0312c8db56ca84c
HalfTenshi <irsyadhifdilhq2119>
2024-09-25 21:39:12 +0700

    mengubah format dari php ke html agar bisa di upload ke github

--- database.php
+++ database.php
@@ -1,13 +1,13 @@
 <?php 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database_name = "tugass3t2";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database_name = "tugass3t2";
 
-$conn = mysqli_connect($hostname,$username,$password,$database_name);
+// $conn = mysqli_connect($hostname,$username,$password,$database_name);
 
-if($conn->connect_error){
-    echo "databaserusak";
-    die("error");
-}
+// if($conn->connect_error){
+//     echo "databaserusak";
+//     die("error");
+// }
 ?>
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-function showNotification() {
-    const notification = document.getElementById('notification');
+// function showNotification() {
+//     const notification = document.getElementById('notification');
     
-    // Tampilkan notifikasi
-    notification.classList.add('show');
-    notification.style.display = "block"; // Pastikan elemen terlihat
+//     // Tampilkan notifikasi
+//     notification.classList.add('show');
+//     notification.style.display = "block"; // Pastikan elemen terlihat
 
-    // Hapus notifikasi setelah 3 detik
-    setTimeout(function() {
-        notification.classList.remove('show');
-        notification.style.display = "none"; // Sembunyikan elemen setelah dihapus
-    }, 3000);
+//     // Hapus notifikasi setelah 3 detik
+//     setTimeout(function() {
+//         notification.classList.remove('show');
+//         notification.style.display = "none"; // Sembunyikan elemen setelah dihapus
+//     }, 3000);
     
-    return true; 
-}
+//     return true; 
+// }
 
-// Ambil parameter URL untuk memeriksa apakah status adalah "success"
-const urlParams = new URLSearchParams(window.location.search);
-const status = urlParams.get('status');
+// // Ambil parameter URL untuk memeriksa apakah status adalah "success"
+// const urlParams = new URLSearchParams(window.location.search);
+// const status = urlParams.get('status');
 
-if (status === 'success') {
-    showNotification();
-}
+// if (status === 'success') {
+//     showNotification();
+// }
